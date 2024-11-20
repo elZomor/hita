77b803b87ae42315bbb20fb3032c7b75783a4dd4
hossam <hossam.salem667@gmail.com>
2024-11-20 20:36:37 +0200

    fix: general comments

--- src/components/homeScreen/HomeScreen.tsx
+++ src/components/homeScreen/HomeScreen.tsx
@@ -1,26 +1,29 @@
-import {Header} from "../header/Header.tsx";
-import {Filters} from "../filters/Filters.tsx";
-import {actors} from "../../assets/data.ts";
-import {ActorCard} from "../performerCard/PerformerCard.tsx";
+import { Header } from '../header/Header.tsx';
+import { Filters } from '../filters/Filters.tsx';
+import { actors } from '../../assets/data.ts';
+import { ActorCard } from '../performerCard/PerformerCard.tsx';
+import Container from '../container/Container.tsx';
 
 export default function App() {
-    return (
-        <div className="min-h-screen bg-gray-50">
-            <Header />
+  return (
+    <div className="min-h-screen bg-gray-50">
+      <Header />
 
-            <main className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-8">
-                <div className="flex flex-row gap-8">
-                    <Filters />
+      <main className="py-8">
+        <Container>
+          <div className="flex flex-row gap-8">
+            <Filters />
 
-                    <div className="flex-1">
-                        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-6">
-                            {actors.map((actor) => (
-                                <ActorCard key={actor.id} actor={actor} />
-                            ))}
-                        </div>
-                    </div>
-                </div>
-            </main>
-        </div>
-    );
+            <div className="flex-1">
+              <div className="grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-4">
+                {actors.map((actor) => (
+                  <ActorCard key={actor.id} actor={actor} />
+                ))}
+              </div>
+            </div>
+          </div>
+        </Container>
+      </main>
+    </div>
+  );
 }
