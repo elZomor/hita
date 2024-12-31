5e5cdaee19ffac68ab674bd5a6bd5aa10f1b8342
zomor <mohamed.elzomor@outlook.com>
2024-12-31 02:31:34 +0200

    add login and singup on FE

--- src/pages/login/SocialMediaLogin.tsx
+++ src/pages/login/SocialMediaLogin.tsx
@@ -3,6 +3,7 @@ import { useNavigate } from 'react-router-dom';
 import { useAmplitude } from '../../hooks/useAmplitude.tsx';
 import { useTranslation } from 'react-i18next';
 import { GoogleLogin } from '@react-oauth/google';
+import { useEffect } from 'react';
 
 type SocialMediaLoginProps = {
   setIsLoading: (state: boolean) => void;
@@ -21,10 +22,53 @@ const SocialMediaLogin = ({
   const { identifyUser } = useAmplitude();
   const { t } = useTranslation();
 
-  const onSuccessHandler = async (response: any) => {
+  // const onSuccessHandler = async (response: any) => {
+  //   setIsLoading(true);
+  //   try {
+  //     await get_login_token(response.credential);
+  //     identifyUser(getUserId());
+  //     navigate('/landing');
+  //     setSnackbar({
+  //       open: true,
+  //       message: 'Successfully logged in with Google!',
+  //       type: 'success',
+  //     });
+  //   } catch {
+  //     setSnackbar({
+  //       open: true,
+  //       message: 'Failed to login with Google. Please try again.',
+  //       type: 'error',
+  //     });
+  //   } finally {
+  //     setIsLoading(false);
+  //   }
+  // };
+
+  const onErrorHandler = () => {
+    setSnackbar({
+      open: true,
+      message: 'Failed to login with Google. Please try again.',
+      type: 'error',
+    });
+    setIsLoading(false);
+  };
+
+  const element = document.querySelector('span.nsm7Bb-HzV7m-LgbsSe-BPrWId');
+  if (element && element.textContent !== t('LOGIN_PAGE.CONTINUE_WITH_GOOGLE')) {
+    element.textContent = t('LOGIN_PAGE.CONTINUE_WITH_GOOGLE');
+  }
+
+  const handleRedirectCallback = async () => {
     setIsLoading(true);
     try {
-      await get_login_token(response.credential);
+      const urlParams = new URLSearchParams(window.location.search);
+      const code = urlParams.get('code');
+
+      if (!code) {
+        throw new Error('Authorization code not found.');
+      }
+
+      await get_login_token(code); // Exchange the code for a session token
       identifyUser(getUserId());
       navigate('/landing');
       setSnackbar({
@@ -43,23 +87,20 @@ const SocialMediaLogin = ({
     }
   };
 
-  const onErrorHandler = () => {
-    setSnackbar({
-      open: true,
-      message: 'Failed to login with Google. Please try again.',
-      type: 'error',
-    });
-    setIsLoading(false);
-  };
-
-  const element = document.querySelector('span.nsm7Bb-HzV7m-LgbsSe-BPrWId');
-  if (element && element.textContent !== t('LOGIN_PAGE.CONTINUE_WITH_GOOGLE')) {
-    element.textContent = t('LOGIN_PAGE.CONTINUE_WITH_GOOGLE');
-  }
+  useEffect(() => {
+    if (window.location.pathname === '/auth/callback') {
+      handleRedirectCallback();
+    }
+  }, []);
 
   return (
     <div className="flex w-full items-center justify-center">
-      <GoogleLogin onSuccess={onSuccessHandler} onError={onErrorHandler} />
+      <GoogleLogin
+        onSuccess={() => {}}
+        onError={onErrorHandler}
+        ux_mode="redirect"
+        login_uri={'https://hita.eg-theater.online/auth/callback'}
+      />
     </div>
   );
 };
